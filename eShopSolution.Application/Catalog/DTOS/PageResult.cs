8075c22bbba68b82021698da1765cdfd9a34be1c
Sơn Hoàng <sonhv@vacom.com.vn>
2020-11-17 14:32:28 +0700

    Create Page

---
+++ eShopSolution.Application/Catalog/DTOS/PageResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.DTOS
+{
+    public class PageResult<T>
+    {
+        List<T> Items { get; set; }
+        public int TotalRecord { get; set; }
+    }
+}
